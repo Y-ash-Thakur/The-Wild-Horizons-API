3305740f2813128a81547cbedeac3b7cd17bf1e8
Y-ash-Thakur <yasht0509@gmail.com>
2025-08-16 00:27:57 +0530

    Add path parameters, route not found, content-type

--- server.js
+++ server.js
@@ -1,15 +1,38 @@
-import http, { get } from 'node:http';
-import { getDataFromDB } from './database/db.js';
+import http from 'node:http'
+import { getDataFromDB } from './database/db.js'
 
-const PORT = 8000;
-const server = http.createServer( async(req, res) => {
+const PORT = 8000
 
-  const destinations = await getDataFromDB();
+const server = http.createServer(async (req, res) => {
+  const destinations = await getDataFromDB()
 
-  if (req.url === '/api' && req.method === 'GET'){
-    res.end(JSON.stringify(destinations));
+  if (req.url === '/api' && req.method === 'GET') {
+
+    res.setHeader('Content-Type', 'application/json')
+    res.statusCode = 200
+    res.end(JSON.stringify(destinations))
+
+  } else if (req.url.startsWith('/api/continent') && req.method === 'GET') {
+
+    const continent = req.url.split('/').pop()
+    const filteredData = destinations.filter((destination) => {
+      return destination.continent.toLowerCase() === continent.toLowerCase()
+    })
+    res.setHeader('Content-Type', 'application/json')
+    res.statusCode = 200
+    res.end(JSON.stringify(filteredData))
+
+  } else {
+
+    res.setHeader('Content-Type', 'application/json')
+    res.statusCode = 404
+    res.end(JSON.stringify({
+      error: "not found",
+      message: "The requested route does not exist"
+    })
+    )
   }
+  
+})
 
-  })
-
-  server.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
+server.listen(PORT, () => console.log(`Connected on port: ${PORT}`))
