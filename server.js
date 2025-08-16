2d28911ca699cfafe1f4575c0cdc96d72ea762c4
Y-ash-Thakur <yasht0509@gmail.com>
2025-08-16 12:43:27 +0530

    Modularised The Code

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 import http from 'node:http'
 import { getDataFromDB } from './database/db.js'
+import { sendJOSNResponse } from './utils/sendJSONResponse.js'
+import { getDataByPathParams } from './utils/getDataByPathParams.js'
 
 const PORT = 8000
 
@@ -7,30 +9,28 @@ const server = http.createServer(async (req, res) => {
   const destinations = await getDataFromDB()
 
   if (req.url === '/api' && req.method === 'GET') {
-
-    res.setHeader('Content-Type', 'application/json')
-    res.statusCode = 200
-    res.end(JSON.stringify(destinations))
+    sendJOSNResponse(res, 200, destinations)
 
   } else if (req.url.startsWith('/api/continent') && req.method === 'GET') {
 
     const continent = req.url.split('/').pop()
-    const filteredData = destinations.filter((destination) => {
-      return destination.continent.toLowerCase() === continent.toLowerCase()
-    })
-    res.setHeader('Content-Type', 'application/json')
-    res.statusCode = 200
-    res.end(JSON.stringify(filteredData))
+    const filteredData = getDataByPathParams(destinations, 'continent', continent)
+    sendJOSNResponse(res, 200, filteredData)
 
-  } else {
+  } else if (req.url.startsWith('/api/country') && req.method === 'GET') {
 
-    res.setHeader('Content-Type', 'application/json')
-    res.statusCode = 404
-    res.end(JSON.stringify({
-      error: "not found",
-      message: "The requested route does not exist"
-    })
-    )
+    const country = req.url.split('/').pop()
+    const filteredData = getDataByPathParams(destinations, 'country', country)
+    sendJOSNResponse(res, 200, filteredData)
+
+  }
+  
+  else {
+
+    sendJOSNResponse(res, 404, ({
+      error: 'Not Found',
+      message: 'The requested route does not exist.'
+    }))
   }
   
 })
